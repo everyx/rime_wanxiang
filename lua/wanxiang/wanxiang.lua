63c48547b58413138951170da90e87465c36590b
amzxyz <rcm@qq.com>
2026-07-03 13:44:14 +0800

    当决定了预测功能只应用于移动端的情形下，现在简化为输入中任意按键都能打断预测，只能靠点击上屏预测候选。其次移除了空格设置，在移动端使用了所以空格上屏空格成了必然因此就不用设置了

--- lua/wanxiang/wanxiang.lua
+++ lua/wanxiang/wanxiang.lua
@@ -37,6 +37,7 @@ function wanxiang.is_mobile_device()
             lower_dist == "hamster3" or
             lower_dist == "default" or --超越
             lower_dist == "xime" or --曦码
+            lower_dist == "lyraime" or  --灵韵
             lower_dist == "squirrel" then --特殊情况
             return true
         end
